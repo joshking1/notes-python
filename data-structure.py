183946667b2b17b6d2e1139e3ff76d9d7a071610
joshking1 <88409463+joshking1@users.noreply.github.com>
2023-06-27 18:52:17 -0400

    Create data-structure.py

---
+++ data-structure.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+x = 5  # this is a regulat variable that is used to store just one value
+fruits = ['mango','banana','pineapple','berry','sugarcane'] # This is also a variable
+
+# We have different types of data structures in python
+# Data structures in python are used to store collection of data
+# There are four built in data structures in python 
+# 1. List ==>[]
+# 2. tuple ==>()
+# 3. Dictionary ==>{} with key value pairs 
+# 4. Set ==>{}
